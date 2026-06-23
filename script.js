6dbcc7a7ecc8e13651bd9fc3d1086c388fb3a3b7
singhsanskriti016 <singh.sanskriti0016@gmail.com>
2026-06-23 22:51:20 +0530

    SyncUp frontend completed

--- script.js
+++ script.js
@@ -111,3 +111,134 @@ function createPostCard(text, likes) {
     });
 
 }
+document.querySelectorAll(".likeBtn").forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+        let count = btn.querySelector(".count");
+
+        let current = parseInt(count.innerText);
+
+        count.innerText = current + 1;
+
+    });
+
+});
+document.querySelectorAll(".commentBtn").forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+        let comment = prompt("Enter your comment:");
+
+        if(comment){
+
+            const post =
+            btn.closest(".post");
+
+            const commentBox =
+            document.createElement("p");
+
+            commentBox.innerHTML =
+            "💬 " + comment;
+
+            commentBox.style.marginTop = "10px";
+
+            commentBox.style.color = "#ff4f9a";
+
+            post.appendChild(commentBox);
+
+        }
+
+    });
+
+});
+const logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", () => {
+
+    let confirmLogout = confirm("Do you want to logout?");
+
+    if(confirmLogout){
+        alert("Logged Out Successfully");
+        location.reload();
+    }
+
+});
+const profileImg = document.querySelector(".navIcons img");
+const profilePopup = document.getElementById("profilePopup");
+const closePopup = document.getElementById("closePopup");
+
+profileImg.addEventListener("click", () => {
+    profilePopup.style.display = "flex";
+});
+
+closePopup.addEventListener("click", () => {
+    profilePopup.style.display = "none";
+});
+const searchInput =
+document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", () => {
+
+    let value =
+    searchInput.value.toLowerCase();
+
+    let posts =
+    document.querySelectorAll(".post");
+
+    posts.forEach(post => {
+
+        let text =
+        post.innerText.toLowerCase();
+
+        if(text.includes(value)){
+
+            post.style.display = "block";
+
+        }else{
+
+            post.style.display = "none";
+
+        }
+
+    });
+
+});
+const editProfileBtn =
+document.getElementById("editProfileBtn");
+
+const usernameText =
+document.getElementById("usernameText");
+
+editProfileBtn.addEventListener("click", () => {
+
+    let newName =
+    prompt("Enter new username:");
+
+    if(newName){
+
+        usernameText.innerText =
+        newName;
+
+        localStorage.setItem(
+            "username",
+            newName
+        );
+
+    }
+
+});
+
+window.addEventListener("load", () => {
+
+    let savedName =
+    localStorage.getItem("username");
+
+    if(savedName){
+
+        usernameText.innerText =
+        savedName;
+
+    }
+
+});
